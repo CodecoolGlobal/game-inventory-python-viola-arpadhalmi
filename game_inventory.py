a1d1c3682deb0588c8e774a8d3c3185e36bf1cc0
viola-arpadhalmi <95080065+viola-arpadhalmi@users.noreply.github.com>
2022-01-16 15:00:10 +0100

    implement inventory export and import

--- game_inventory.py
+++ game_inventory.py
@@ -1,10 +1,5 @@
-
-# This is the file where you must work.
-# Write code in the functions (and create new functions) so that they work
-# according to the requirements.
-
-
 from operator import inv
+import csv
 
 
 def display_inventory(inventory):
@@ -42,23 +37,41 @@ def print_table(inventory, order=None):
     print(f'{DIVIDER}\n{HEADER}\n{DIVIDER}')
     if order == 'count,asc':
         for item, count in sorted(inventory).reverse():
-            print(f'{item}\t|\t{count}')
+            print(f'{item} |\t{count}')
     elif order == 'count,desc':
-        for item, count in sorted(inventory.items()):
-            print(f'{item}\t|\t{count}')
+        for item, count in inventory.sort:
+            print(f'{item} |\t{count}')
     else:
         for item, count in inventory.items():
-            print(f'{item}\t|\t{count}')
+            print(f'{item} |\t{count}')
     print(f'{DIVIDER}')
 
 
-def import_inventory(inventory, filename):
+def import_inventory(inventory, filename='import_inventory.csv'):
     """Import new inventory items from a CSV file."""
+    try:
+        with open(filename, 'r') as csvfile:
+            inventory_reader = csv.reader(csvfile)
+            for row in inventory_reader:
+                for i in row:
+                    add_to_inventory(inventory, i)
+    except FileNotFoundError:
+        print(f"File '{filename}' not found!")
 
-    pass
 
-
-def export_inventory(inventory, filename):
+def export_inventory(inventory, filename='export_inventory.csv'):
     """Export the inventory into a CSV file."""
+    inventory_list = []
+    for items, count in inventory.items():
+        inventory_list.extend([items for i in range(count)])
+    print(inventory_list)
+    try:
+        with open(filename, 'w') as export:
+            writer = csv.writer(export)
+            writer.writerow(inventory_list)
+    except PermissionError:
+        print(f"You don't have permission creating file '{filename}'!")
 
-    pass
+inventory = {}
+import_inventory(inventory, 'test_inventory.csv')
+export_inventory(inventory)
